3572f4f2781dca7df85cb8324a47052590da713c
Akihisa Higuchi <akihisa.nq@gmail.com>
2016-09-06 00:00:00 +0900

    Custom Tab を使うように変更

--- app/src/main/java/net/nqlab/simple/MainActivity.java
+++ app/src/main/java/net/nqlab/simple/MainActivity.java
@@ -1,6 +1,7 @@
 package net.nqlab.simple;
 
 import android.support.v7.app.ActionBarActivity;
+import android.support.customtabs.CustomTabsIntent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -37,7 +38,7 @@ import net.nqlab.btmw.AccessToken;
 import net.nqlab.sample.ServerInfo;
 
 public class MainActivity extends ActionBarActivity {
-	RestAdapter m_adapter = null;
+    private RestAdapter m_adapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,45 +51,50 @@ public class MainActivity extends ActionBarActivity {
                 .registerTypeAdapter(Date.class, new DateTypeAdapter())
                 .create();
  
-		// PIN コード取得
-		Uri uri = Uri.parse(
-			ServerInfo.URL_BASE
-				+ "/oauth/authorize?"
-				+ "client_id=" + ServerInfo.CLIENT_ID
-				+ "&redirect_uri=" + java.net.URLEncoder.encode(ServerInfo.REDIRECT_URL)
-				+ "&response_type=code"
-				+ "&scope="
-			);
-		Intent i = new Intent(Intent.ACTION_VIEW,uri);
-		startActivity(i);
+        // PIN コード取得
+        Uri uri = Uri.parse(
+            ServerInfo.URL_BASE
+                + "/oauth/authorize?"
+                + "client_id=" + ServerInfo.CLIENT_ID
+                + "&redirect_uri=" + java.net.URLEncoder.encode(ServerInfo.REDIRECT_URL)
+                + "&response_type=code"
+                + "&scope="
+            );
+        final CustomTabsIntent tabsIntent = new CustomTabsIntent.Builder()
+            .setShowTitle(true)
+            // .setToolbarColor(ContextCompat.getColor(this, R.color.primary))
+            // .setStartAnimations(this, R.anim.slide_in_right, R.anim.slide_out_left)
+            // .setExitAnimations(this, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+            .build();
+        tabsIntent.launchUrl(this, uri);
 
-		//テキスト入力を受け付けるビューを作成します。
-		final EditText editView = new EditText(MainActivity.this);
-		new AlertDialog.Builder(MainActivity.this)
-			.setIcon(android.R.drawable.ic_dialog_info)
-			.setTitle("テキスト入力ダイアログ")
-			//setViewにてビューを設定します。
-			.setView(editView)
-			.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int whichButton) {
-					String code = editView.getText().toString();
+        //テキスト入力を受け付けるビューを作成します。
+        final EditText editView = new EditText(MainActivity.this);
+        new AlertDialog.Builder(MainActivity.this)
+            .setIcon(android.R.drawable.ic_dialog_info)
+            .setTitle("テキスト入力ダイアログ")
+            //setViewにてビューを設定します。
+            .setView(editView)
+            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String code = editView.getText().toString();
 
-					// RestAdapterの生成
-					RestAdapter adapter = new RestAdapter.Builder()
-						.setEndpoint(ServerInfo.URL_BASE)
-						.setConverter(new GsonConverter(gson))
-						.setLogLevel(RestAdapter.LogLevel.FULL)
-						.setLog(new AndroidLog("=NETWORK="))
-						.build();
+                    // RestAdapterの生成
+                    RestAdapter adapter = new RestAdapter.Builder()
+                        .setEndpoint(ServerInfo.URL_BASE)
+                        .setConverter(new GsonConverter(gson))
+                        .setLogLevel(RestAdapter.LogLevel.FULL)
+                        .setLog(new AndroidLog("=NETWORK="))
+                        .build();
 
-					// アクセス トークン取得
-					adapter.create(LoginApi.class).getAccessToken(
-							"authorization_code",
-							ServerInfo.CLIENT_ID,
-							ServerInfo.CLIENT_SECRET,
-							code,
-							ServerInfo.REDIRECT_URL
-							)
+                    // アクセス トークン取得
+                    adapter.create(LoginApi.class).getAccessToken(
+                            "authorization_code",
+                            ServerInfo.CLIENT_ID,
+                            ServerInfo.CLIENT_SECRET,
+                            code,
+                            ServerInfo.REDIRECT_URL
+                            )
                         .subscribeOn(Schedulers.newThread())
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(new Observer<AccessToken>() {
@@ -106,38 +112,38 @@ public class MainActivity extends ActionBarActivity {
                             public void onNext(AccessToken token) {
                                 Log.d("MainActivity", "onNext()");
                                 if (token != null) {
-									final String accessToken = token.getAccessToken();
+                                    final String accessToken = token.getAccessToken();
 
-									m_adapter = new RestAdapter.Builder()
-										.setEndpoint(ServerInfo.URL_BASE)
-										.setConverter(new GsonConverter(gson))
-										.setLogLevel(RestAdapter.LogLevel.FULL)
-										.setLog(new AndroidLog("=NETWORK="))
-										.setRequestInterceptor(new RequestInterceptor() {
-												@Override
-												public void intercept(RequestInterceptor.RequestFacade request) {
-													request.addHeader("Authorization", "Bearer " + accessToken);
-												}
-											})
-										.build();
+                                    m_adapter = new RestAdapter.Builder()
+                                        .setEndpoint(ServerInfo.URL_BASE)
+                                        .setConverter(new GsonConverter(gson))
+                                        .setLogLevel(RestAdapter.LogLevel.FULL)
+                                        .setLog(new AndroidLog("=NETWORK="))
+                                        .setRequestInterceptor(new RequestInterceptor() {
+                                                @Override
+                                                public void intercept(RequestInterceptor.RequestFacade request) {
+                                                    request.addHeader("Authorization", "Bearer " + accessToken);
+                                                }
+                                            })
+                                        .build();
                                 }
                             }
                         });
-				}
-			})
-			.setNegativeButton("キャンセル", new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int whichButton) {
-				}
-			})
-			.show();
+                }
+            })
+            .setNegativeButton("キャンセル", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                }
+            })
+            .show();
 
-		// 要求に応じて API 実行
+        // 要求に応じて API 実行
         Button button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-				if (m_adapter == null) {
-					return;
-				}
+                if (m_adapter == null) {
+                    return;
+                }
 
                 // 非同期処理の実行
                 m_adapter.create(ExclusionAreaApi.class).list()
@@ -186,5 +192,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
