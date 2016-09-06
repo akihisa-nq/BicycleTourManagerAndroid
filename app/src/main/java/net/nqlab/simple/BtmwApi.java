3107c870c74d20b3cb4d8448b863e2dd2d2cca65
Akihisa Higuchi <akihisa.nq@gmail.com>
2016-09-07 00:00:00 +0900

    Api アクセスまわりを分離

---
+++ app/src/main/java/net/nqlab/simple/BtmwApi.java
@@ -0,0 +1,119 @@
+package net.nqlab.simple;
+
+import java.util.Date;
+
+import android.net.Uri;
+
+import net.nqlab.btmw.ExclusionAreaApi;
+import net.nqlab.btmw.ExclusionAreaList;
+import net.nqlab.btmw.LoginApi;
+import net.nqlab.btmw.AccessToken;
+
+import retrofit.RestAdapter;
+import retrofit.RequestInterceptor;
+import retrofit.android.AndroidLog;
+import retrofit.converter.GsonConverter;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.internal.bind.DateTypeAdapter;
+
+import net.nqlab.simple.ServerInfo;
+import net.nqlab.simple.SecureSaveData;
+import net.nqlab.simple.SaveData;
+
+public class BtmwApi {
+    private RestAdapter mAdapter = null;
+    private SecureSaveData mSecureSaveData;
+    private SaveData mSaveData;
+
+    public BtmwApi(SecureSaveData secureSaveData, SaveData saveData) {
+        mSecureSaveData = secureSaveData;
+        mSaveData = saveData;
+    }
+
+    public void login(String code) {
+        // JSONのパーサー
+        final Gson gson = new GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .registerTypeAdapter(Date.class, new DateTypeAdapter())
+            .create();
+
+        // RestAdapterの生成
+        RestAdapter adapter = new RestAdapter.Builder()
+            .setEndpoint(ServerInfo.URL_BASE)
+            .setConverter(new GsonConverter(gson))
+            .setLogLevel(RestAdapter.LogLevel.FULL)
+            .setLog(new AndroidLog("=NETWORK="))
+            .build();
+
+        // アクセス トークン取得
+        adapter.create(LoginApi.class).getAccessToken(
+                "authorization_code",
+                ServerInfo.CLIENT_ID,
+                ServerInfo.CLIENT_SECRET,
+                code,
+                ServerInfo.REDIRECT_URL
+                )
+            .subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new Observer<AccessToken>() {
+                @Override
+                public void onCompleted() {
+                }
+
+                @Override
+                public void onError(Throwable e) {
+                }
+
+                @Override
+                public void onNext(AccessToken token) {
+                    if (token != null) {
+                        final String accessToken = token.getAccessToken();
+                        mSaveData.save("token", mSecureSaveData.encryptString(accessToken));
+
+                        mAdapter = new RestAdapter.Builder()
+                            .setEndpoint(ServerInfo.URL_BASE)
+                            .setConverter(new GsonConverter(gson))
+                            .setLogLevel(RestAdapter.LogLevel.FULL)
+                            .setLog(new AndroidLog("=NETWORK="))
+                            .setRequestInterceptor(new RequestInterceptor() {
+                                    @Override
+                                    public void intercept(RequestInterceptor.RequestFacade request) {
+                                        request.addHeader("Authorization", "Bearer " + accessToken);
+                                    }
+                                })
+                            .build();
+                    }
+                }
+            });
+    }
+
+    public Uri getLoginUri()
+    {
+        Uri uri = Uri.parse(
+            ServerInfo.URL_BASE
+                + "/oauth/authorize?"
+                + "client_id=" + ServerInfo.CLIENT_ID
+                + "&redirect_uri=" + java.net.URLEncoder.encode(ServerInfo.REDIRECT_URL)
+                + "&response_type=code"
+                + "&scope="
+            );
+        return uri;
+    }
+
+    public boolean isLogin()
+    {
+        return mAdapter != null;
+    }
+
+    public ExclusionAreaApi getExclusionAreaApi()
+    {
+        if (mAdapter == null) { return null; }
+        return mAdapter.create(ExclusionAreaApi.class);
+    }
+}
--- app/src/main/java/net/nqlab/simple/MainActivity.java
+++ app/src/main/java/net/nqlab/simple/MainActivity.java
@@ -13,36 +13,20 @@ import android.widget.EditText;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.internal.bind.DateTypeAdapter;
- 
-import java.util.Date;
- 
-import retrofit.RestAdapter;
-import retrofit.RequestInterceptor;
-import retrofit.android.AndroidLog;
-import retrofit.converter.GsonConverter;
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
-
-import net.nqlab.btmw.ExclusionAreaApi;
 import net.nqlab.btmw.ExclusionAreaList;
-import net.nqlab.btmw.LoginApi;
-import net.nqlab.btmw.AccessToken;
 
-import net.nqlab.simple.ServerInfo;
 import net.nqlab.simple.SecureSaveData;
 import net.nqlab.simple.SaveData;
+import net.nqlab.simple.BtmwApi;
 
 public class MainActivity extends ActionBarActivity {
-    private RestAdapter mAdapter = null;
     private SecureSaveData mSecureSaveData = null;
     private SaveData mSaveData;
+    private BtmwApi mApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,29 +35,16 @@ public class MainActivity extends ActionBarActivity {
 
         mSecureSaveData = new SecureSaveData();
         mSaveData = new SaveData(getApplicationContext());
-
-        // JSONのパーサー
-        final Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                .registerTypeAdapter(Date.class, new DateTypeAdapter())
-                .create();
+		mApi = new BtmwApi(mSecureSaveData, mSaveData);
  
         // PIN コード取得
-        Uri uri = Uri.parse(
-            ServerInfo.URL_BASE
-                + "/oauth/authorize?"
-                + "client_id=" + ServerInfo.CLIENT_ID
-                + "&redirect_uri=" + java.net.URLEncoder.encode(ServerInfo.REDIRECT_URL)
-                + "&response_type=code"
-                + "&scope="
-            );
         final CustomTabsIntent tabsIntent = new CustomTabsIntent.Builder()
             .setShowTitle(true)
             // .setToolbarColor(ContextCompat.getColor(this, R.color.primary))
             // .setStartAnimations(this, R.anim.slide_in_right, R.anim.slide_out_left)
             // .setExitAnimations(this, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
             .build();
-        tabsIntent.launchUrl(this, uri);
+        tabsIntent.launchUrl(this, mApi.getLoginUri());
 
         //テキスト入力を受け付けるビューを作成します。
         final EditText editView = new EditText(MainActivity.this);
@@ -85,58 +56,7 @@ public class MainActivity extends ActionBarActivity {
             .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int whichButton) {
                     String code = editView.getText().toString();
-
-                    // RestAdapterの生成
-                    RestAdapter adapter = new RestAdapter.Builder()
-                        .setEndpoint(ServerInfo.URL_BASE)
-                        .setConverter(new GsonConverter(gson))
-                        .setLogLevel(RestAdapter.LogLevel.FULL)
-                        .setLog(new AndroidLog("=NETWORK="))
-                        .build();
-
-                    // アクセス トークン取得
-                    adapter.create(LoginApi.class).getAccessToken(
-                            "authorization_code",
-                            ServerInfo.CLIENT_ID,
-                            ServerInfo.CLIENT_SECRET,
-                            code,
-                            ServerInfo.REDIRECT_URL
-                            )
-                        .subscribeOn(Schedulers.newThread())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(new Observer<AccessToken>() {
-                            @Override
-                            public void onCompleted() {
-                                Log.d("MainActivity", "onCompleted()");
-                            }
-
-                            @Override
-                            public void onError(Throwable e) {
-                                Log.e("MainActivity", "Error : " + e.toString());
-                            }
-
-                            @Override
-                            public void onNext(AccessToken token) {
-                                Log.d("MainActivity", "onNext()");
-                                if (token != null) {
-                                    final String accessToken = token.getAccessToken();
-                                    mSaveData.save("token", mSecureSaveData.encryptString(accessToken));
-
-                                    mAdapter = new RestAdapter.Builder()
-                                        .setEndpoint(ServerInfo.URL_BASE)
-                                        .setConverter(new GsonConverter(gson))
-                                        .setLogLevel(RestAdapter.LogLevel.FULL)
-                                        .setLog(new AndroidLog("=NETWORK="))
-                                        .setRequestInterceptor(new RequestInterceptor() {
-                                                @Override
-                                                public void intercept(RequestInterceptor.RequestFacade request) {
-                                                    request.addHeader("Authorization", "Bearer " + accessToken);
-                                                }
-                                            })
-                                        .build();
-                                }
-                            }
-                        });
+					mApi.login(code);
                 }
             })
             .setNegativeButton("キャンセル", new DialogInterface.OnClickListener() {
@@ -149,14 +69,14 @@ public class MainActivity extends ActionBarActivity {
         Button button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if (mAdapter == null) {
+                if (! mApi.isLogin()) {
                     return;
                 }
 
                 // 非同期処理の実行
-                mAdapter.create(ExclusionAreaApi.class).list()
-                    .subscribeOn(Schedulers.newThread())
-                    .observeOn(AndroidSchedulers.mainThread())
+                mApi.getExclusionAreaApi().list()
+					.subscribeOn(Schedulers.newThread())
+					.observeOn(AndroidSchedulers.mainThread())
                     .subscribe(new Observer<ExclusionAreaList>() {
                         @Override
                         public void onCompleted() {
@@ -172,7 +92,9 @@ public class MainActivity extends ActionBarActivity {
                         public void onNext(ExclusionAreaList list) {
                             Log.d("MainActivity", "onNext()");
                             if (list != null) {
-                                ((TextView) findViewById(R.id.textView)).setText(list.getExclusionAreas().get(0).getPoint());
+                                ((TextView) findViewById(R.id.textView)).setText(
+									list.getExclusionAreas().get(0).getPoint()
+									);
                             }
                         }
                     });
